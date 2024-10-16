71bddb9459c095412982dd3299e9ac53beaa72f2
Felipe Pedroso <f.pedroso677@gmail.com>
2024-10-16 16:59:01 +0100

    Adding spotless to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,36 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+
 plugins {
     alias(libs.plugins.androidGradlePlugin) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply<SpotlessPlugin>()
+
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            ktlint()
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+
+        kotlinGradle {
+            target("**/*.kts")
+            ktlint()
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+    }
 }
 
 tasks.register("clean", Delete::class) {
